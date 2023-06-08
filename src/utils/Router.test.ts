231d8be7d8abfdcbda598984b4a8859e3dab9569
Oleg Ilin <ilindev78@gmal.com>
2023-06-08 04:02:42 +0300

    upd добавил тесты для HTTPTransport Router set Block Button

---
+++ src/utils/Router.test.ts
@@ -0,0 +1,30 @@
+import Router, { BlockConstructable } from './Router'
+
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+describe('Router', () => {
+
+  global.window.history.back = () => {
+    if (typeof window.onpopstate === 'function') {
+      window.onpopstate({currentTarget: window} as unknown as PopStateEvent);
+    }
+  };
+  global.window.history.forward = () => {
+    if (typeof window.onpopstate === 'function') {
+      window.onpopstate({currentTarget: window} as unknown as PopStateEvent);
+    }
+  }
+
+  const getContentFake = sinon.fake.returns(document.createElement('div'));
+
+  const BlockMock = class {
+    getContent = getContentFake;
+  } as unknown as BlockConstructable;
+
+  it('use() должен вернуть экземпляр Router', () => {
+    const result = Router.use('/', BlockMock);
+
+    expect(result).to.eq(Router);
+  });
+});
--- src/utils/Router.ts
+++ src/utils/Router.ts
@@ -1,6 +1,6 @@
 import Block from './Block';
 
-interface BlockConstructable<P extends Record<string, any> = any> {
+export interface BlockConstructable<P extends Record<string, any> = any> {
   new (props: P): Block<P>
 }
 
